5671449eb86ff3748dfd87bfa836dbea638715a5
andyfiveeleven <fouracesguitar@gmail.com>
2017-05-15 13:39:21 -0700

    created objects of all data, added cups per hour

---
+++ app.js
@@ -0,0 +1,39 @@
+
+
+
+var firstAndPike = {
+  minCust: 23,
+  maxCust: 65,
+  avgCookieSale: 6.3,
+  cupsPerHour: Math.floor(Math.random() * (maxCust-minCust) + minCust)
+};
+
+var seaTac = {
+  minCust: 3,
+  maxCust: 24,
+  avgCookieSale: 1.2,
+  cupsPerHour: Math.floor(Math.random() * (maxCust-minCust) + minCust)
+};
+
+var seattleCenter = {
+  minCust: 11,
+  maxCust: 38,
+  avgCookieSale: 2.7
+};
+
+var capHill = {
+  minCust: 20,
+  maxCust: 38,
+  avgCookieSale: 2.3
+};
+
+var alki = {
+  minCust: 2,
+  maxCust: 16,
+  avgCookieSale: 4.6
+};
+
+function salmonSalesNumbers() {
+  var container = document.createElement('div'); //<div></div>
+  container.innerHTML =
+}

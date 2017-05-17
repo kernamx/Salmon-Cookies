d9d86a26da8acf576e4a06d3e272e9ebd1b3ee3d
andyfiveeleven <fouracesguitar@gmail.com>
2017-05-16 17:29:14 -0700

    fixed linter errors in JS

--- app.js
+++ app.js
@@ -3,6 +3,8 @@
 var thead = document.getElementById('thead');
 var headValues = ['', '6:00 AM', '7:00 AM', '8:00 AM', '9:00 AM', '10:00 AM', '11:00 AM', '12:00 AM', '1:00 PM', '2:00 PM', '3:00 PM','4:00 PM', '5:00 PM', '6:00 PM', '7:00 PM', 'daily location totals'];
 
+var new_row;
+
 for (var j = 0; j < headValues.length; j++) {
   new_row = document.createElement('td');
   new_row.innerHTML = headValues[j];

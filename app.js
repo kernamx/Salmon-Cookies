493703564ed27f06c82c67cf7ba3b493c3ebdcef
andyfiveeleven <fouracesguitar@gmail.com>
2017-05-17 17:35:12 -0700

    added clarifying comments

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ function Store(name, minCust, maxCust, avgCookieSale){
   this.maxCust = maxCust;
   this.avgCookieSale = avgCookieSale;
   this.hourlySales = ['<td>' + name + '</td>'];
+  this.cleanSales = []; //thinking about using this for daily totals
 }
 
 Store.prototype.custPerHour = function(){
@@ -20,38 +21,46 @@ Store.prototype.custPerHour = function(){
   return customers;
 };
 
+
+//pushes the 13 hours in the day, plus a daily total, to Hourly Sales, with the TD tags
 Store.prototype.salesByHour = function() {
   var total = 0;
   for(var i = 0; i <14; i++){
     var sales = this.custPerHour() * this.avgCookieSale;
     this.hourlySales.push('<td>' + Math.ceil(sales) + '</td>');
+    this.cleanSales.push(Math.ceil(sales));
     total += Math.ceil(sales);
   }
   this.hourlySales.push('<td>' + total + '</td>');
+  this.cleanSales.push(total);
 };
 
+// renders the store on the page, joins my big array together into a string.
 Store.prototype.render = function(){
   var row = document.createElement('tr');
   row.innerHTML = this.hourlySales.join('');
   table.appendChild(row);
 };
 
+// existing stores, not needed to add
 var firstAndPike = new Store('1st and Pike', 23, 65, 6.3);
 var seaTac = new Store('SeaTac Airport', 3, 24,1.2);
 var seattleCenter = new Store('Seattle Center', 11, 38, 2.7);
 var capHill = new Store('Capitol Hill', 20, 38, 2.3);
 var alki = new Store('Alki', 3, 16, 4.6);
 
-var locations = [firstAndPike, seaTac, seattleCenter, capHill, alki];
+var locations = [firstAndPike, seaTac, seattleCenter, capHill, alki]; //thinking about using this for a daily total line
 
+//makes the salesByHour function run so it generates an hourlySales total
 function getSalesRender(){
-  for (var k = 0; k < locations.length; k++){
+  for (var k = 0; k < 5; k++){
     locations[k].salesByHour();
     locations[k].render();
   }
 }
 getSalesRender();
 
+//this is the store making function, all of our form fields pushing to an object
 function makeStore(event){
   event.preventDefault();
   var name = event.target.name.value;
@@ -61,8 +70,30 @@ function makeStore(event){
   var addStore = new Store(name, minCust, maxCust, avgCookieSale);
   addStore.salesByHour();
   addStore.render();
-  console.log('created a store');
+  locations.push(addStore);
+  console.log(locations);
   form.reset();
 }
 
 form.addEventListener('submit', makeStore);
+
+//oooooooooo experiements, trying to figure out how to create a grand total list at the bottom of my table.
+var grandTotal = [];
+for (var m = 0; m<15; m++) {
+  var hourTotal;
+  for (var j = 1; j < location.length; j++){
+    hourTotal += locations[j].hourlySales[m];
+  }
+  grandTotal.push(hourTotal);
+}
+
+console.log(grandTotal);
+
+// // console.log(grandTotal);
+//
+// var grandDataList = '<td>' + 'totals' + '</td>';  //<-- for loop that itterates over the grandTotal array to add to the data array, but with <td>'s
+// for (var x = 0; x < 15; x++){
+//   grandDataList = grandDataList +'<td>' + grandTotal[x] + '</td>';
+// }
+//
+// data.push(grandDataList);
